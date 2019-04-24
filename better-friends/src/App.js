578aa660ee48c63f988f0a31b40fcd07942c7ad7
ohheyimjen <jendority@gmail.com>
2019-04-24 08:47:01 -0700

    In App.js; Mounted Better Friends api and added contructor

--- better-friends/src/App.js
+++ better-friends/src/App.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import './App.css';
-import Login from './components/Login';
+import React from "react";
+import "./App.css";
+import Login from "./components/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <strong>Better Friend </strong>
-       <p>- an app that makes you seem like a better friend by making you a lazier one <span>👻</span></p>
-      </header>
-      <Login />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://better-friend-server.herokuapp.com/dates/:user_id")
+      .then(res => {
+        console.log(response);
+        this.setState({
+          friends: res.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  updateFriends = friends => {
+    this.setState({
+      friends
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <strong>Better Friend </strong>
+          <p>
+            - an app that makes you seem like a better friend by making you a
+            lazier one <span>👻</span>
+          </p>
+        </header>
+        <Login />
+      </div>
+    );
+  }
 }
 
 export default App;
